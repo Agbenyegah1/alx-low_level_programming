15419933d272cac6adcbc722bbe26ff650979102
Agbenyegah1 <kofysmile123@gmail.com>
2023-08-14 08:06:16 -0700

    Write a program that prints all possible different combinations of three digits. Numbers must be separated by ,, followed by a space The three digits must be different 012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1 and 2 Print only the smallest combination of three digits Numbers should be printed in ascending order, with three digits You can only use the putchar function (every other function (printf, puts, etc…) is forbidden) You can only use putchar six times maximum in your code You are not allowed to use any variable of type char All your code should be in the main function

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,35 +10,35 @@
  */
 int main(void)
 {
-    int num1 = 0;
+	int num1 = 0;
 
-    while (num1 < 10)
-    {
-        int num2 = num1 + 1;
+	while (num1 < 10)
+	{
+		int num2 = num1 + 1;
 
-        while (num2 < 10)
-        {
-            int num3 = num2 + 1;
+		while (num2 < 10)
+		{
+			int num3 = num2 + 1;
 
-            while (num3 < 10)
-            {
-                putchar(num1 + '0');
-                putchar(num2 + '0');
-                putchar(num3 + '0');
+			while (num3 < 10)
+			{
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				putchar(num3 + '0');
 
-                if (num1 < 7)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-                num3++;
-            }
-            num2++;
-        }
-        num1++;
-    }
+				if (num1 < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				num3++;
+			}
+			num2++;
+		}
+		num1++;
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
