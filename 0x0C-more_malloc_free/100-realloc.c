a514c249792086f055481ba04c9842689e55d1a8
Agbenyegah1 <kofysmile123@gmail.com>
2023-11-24 09:50:47 -0800

    malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,39 +1,42 @@
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * _realloc - reallocates a memory block using malloc and free
+ * _realloc - Reallocates a memory block using malloc and free.
+ * @ptr: Pointer to the memory previously allocated with malloc.
+ * @old_size: Size, in bytes, of the allocated space for ptr.
+ * @new_size: New size, in bytes, of the new memory block.
  *
- * @ptr: pointer to the memory previously allocated
- * @old_size:is the size, in bytes, of the allocated space for ptr
- * @new_size: the new size, in bytes of the new memory block
- *
- * Return: pointer allocate new size memory, or NULL
+ * Return: A pointer to the newly allocated memory.
+ *         If new_size is equal to old_size, returns ptr.
+ *         If new_size is zero and ptr is not NULL, returns NULL and frees ptr.
+ *         If ptr is NULL, the call is equivalent to malloc(new_size).
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p;
-	unsigned int i, n = new_size;
-	char *oldp = ptr;
+	char *new_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return ptr;
 
 	if (ptr == NULL)
-	{
-		p = malloc(new_size);
-		return (p);
-	}
-	else if (new_size == 0)
+		return malloc(new_size);
+
+	if (new_size == 0)
 	{
 		free(ptr);
-		return (NULL);
+		return NULL;
 	}
-	else if (new_size == old_size)
-		return (ptr);
-	p = malloc(new_size);
-	if (p == NULL)
-		return (NULL);
-	if (new_size > old_size)
-		n = old_size;
-	for (i = 0; i < n; i++)
-		p[i] = oldp[i];
+
+	new_ptr = malloc(new_size);
+
+	if (new_ptr == NULL)
+		return NULL;
+
+	for (i = 0; i < old_size && i < new_size; i++)
+		new_ptr[i] = ((char *)ptr)[i];
+
 	free(ptr);
-	return (p);
+
+	return new_ptr;
 }
--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,148 +1,124 @@
-#include "main.h"
+#include <main.h>
+#include <stdlib.h>
 
 /**
- * _print - moves a string one place to the left and prints the string
- * @str: string to move
- * @l: size of string
+ * _isdigit - Checks if a string is composed of digits.
+ * @str: The string to check.
  *
- * Return: void
+ * Return: 1 if all characters are digits, 0 otherwise.
  */
-void _print(char *str, int l)
+int _isdigit(char *str)
 {
-	int i, j;
-
-	i = j = 0;
-	while (i < l)
-	{
-		if (str[i] != '0')
-			j = 1;
-		if (j || i == l - 1)
-			_putchar(str[i]);
-		i++;
-	}
-
-	_putchar('\n');
-	free(str);
+    while (*str)
+    {
+        if (*str < '0' || *str > '9')
+            return 0;
+        str++;
+    }
+    return 1;
 }
 
 /**
- * mul - multiplies a char with a string and places the answer into dest
- * @n: char to multiply
- * @num: string to multiply
- * @num_index: last non NULL index of num
- * @dest: destination of multiplication
- * @dest_index: highest index to start addition
- *
- * Return: pointer to dest, or NULL on failure
+ * _multiply - Multiplies two numbers represented as strings.
+ * @num1: First number.
+ * @num2: Second number.
+ * @result: Array to store the result.
+ * @len1: Length of the first number.
+ * @len2: Length of the second number.
  */
-char *mul(char n, char *num, int num_index, char *dest, int dest_index)
+void _multiply(char *num1, char *num2, char *result, int len1, int len2)
 {
-	int j, k, mul, mulrem, add, addrem;
+    int i, j, carry = 0;
 
-	mulrem = addrem = 0;
-	for (j = num_index, k = dest_index; j >= 0; j--, k--)
-	{
-		mul = (n - '0') * (num[j] - '0') + mulrem;
-		mulrem = mul / 10;
-		add = (dest[k] - '0') + (mul % 10) + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	for (addrem += mulrem; k >= 0 && addrem; k--)
-	{
-		add = (dest[k] - '0') + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	if (addrem)
-	{
-		return (NULL);
-	}
-	return (dest);
+    /* Multiply each digit and add to the result */
+    for (i = len1 - 1; i >= 0; i--)
+    {
+        carry = 0;
+        for (j = len2 - 1; j >= 0; j--)
+        {
+            int prod = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1] + carry;
+            result[i + j + 1] = prod % 10;
+            carry = prod / 10;
+        }
+        result[i] += carry;
+    }
 }
 
 /**
- * check_for_digits - checks the arguments to ensure they are digits
- * @av: pointer to arguments
+ * main - Entry point of the program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line arguments.
  *
- * Return: 0 if digits, 1 if not
+ * Return: 0 on success, 98 on failure.
  */
-int check_for_digits(char **av)
-{
-	int i, j;
-
-	for (i = 1; i < 3; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			if (av[i][j] < '0' || av[i][j] > '9')
-				return (1);
-		}
-	}
-	return (0);
-}
-
-/**
- * init - initializes a string
- * @str: sting to initialize
- * @l: length of strinf
- *
- * Return: void
- */
-void init(char *str, int l)
-{
-	int i;
-
-	for (i = 0; i < l; i++)
-		str[i] = '0';
-	str[i] = '\0';
-}
-
-/**
- * main - multiply two numbers
- * @argc: number of arguments
- * @argv: argument vector
- *
- * Return: zero, or exit status of 98 if failure
- */
-
 int main(int argc, char *argv[])
 {
-	int l1, l2, ln, ti, i;
-	char *a;
-	char *t;
-	char e[] = "Error\n";
+    char *num1, *num2, *result;
+    int len1 = 0, len2 = 0, i;
 
-	if (argc != 3 || check_for_digits(argv))
-	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
-		exit(98);
-	}
-	for (l1 = 0; argv[1][l1]; l1++)
-		;
-	for (l2 = 0; argv[2][l2]; l2++)
-		;
-	ln = l1 + l2 + 1;
-	a = malloc(ln * sizeof(char));
-	if (a == NULL)
-	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
-		exit(98);
-	}
-	init(a, ln - 1);
-	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
-	{
-		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
-		if (t == NULL)
-		{
-			for (ti = 0; e[ti]; ti++)
-				_putchar(e[ti]);
-			free(a);
-			exit(98);
-		}
-	}
-	_print(a, ln - 1);
-	return (0);
+    /* Validate number of arguments */
+    if (argc != 3)
+    {
+        _putchar('E');
+        _putchar('r');
+        _putchar('r');
+        _putchar('o');
+        _putchar('r');
+        _putchar('\n');
+        return 98;
+    }
+
+    num1 = argv[1];
+    num2 = argv[2];
+
+    /* Validate input */
+    if (!_isdigit(num1) || !_isdigit(num2))
+    {
+        _putchar('E');
+        _putchar('r');
+        _putchar('r');
+        _putchar('o');
+        _putchar('r');
+        _putchar('\n');
+        return 98;
+    }
+
+    /* Calculate lengths of input numbers */
+    while (num1[len1])
+        len1++;
+    while (num2[len2])
+        len2++;
+
+    /* Initialize result array with zeros */
+    result = malloc((len1 + len2) * sizeof(char));
+    if (result == NULL)
+    {
+        _putchar('E');
+        _putchar('r');
+        _putchar('r');
+        _putchar('o');
+        _putchar('r');
+        _putchar('\n');
+        return 98;
+    }
+
+    /* Multiply the numbers */
+    _multiply(num1, num2, result, len1, len2);
+
+    /* Print the result */
+    i = 0;
+    while (result[i] == 0 && result[i + 1] != '\0')
+        i++;
+
+    while (result[i])
+    {
+        _putchar(result[i] + '0');
+        i++;
+    }
+    _putchar('\n');
+
+    /* Free allocated memory */
+    free(result);
+
+    return 0;
 }
