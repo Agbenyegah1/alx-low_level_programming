4d60cf0d276edb7dd34fe1a56828d421b5238d7e
Agbenyegah1 <kofysmile123@gmail.com>
2023-11-24 09:02:07 -0800

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdlib.h>
 
 /**
  * string_nconcat - Concatenates two strings up to n bytes.
@@ -11,35 +11,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *concatenated;
-    unsigned int len_s1, len_s2, i, j;
+	char *concatenated;
+	unsigned int len_s1, len_s2, i, j;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
-        ;
+	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
+		;
 
-    for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
-        ;
+	for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
+		;
 
-    if (n >= len_s2)
-        n = len_s2;
+	if (n >= len_s2)
+		n = len_s2;
 
-    concatenated = malloc(sizeof(char) * (len_s1 + n + 1));
+	concatenated = malloc(sizeof(char) * (len_s1 + n + 1));
 
-    if (concatenated == NULL)
-        return NULL;
+	if (concatenated == NULL)
+		return NULL;
 
-    for (i = 0; i < len_s1; i++)
-        concatenated[i] = s1[i];
+	for (i = 0; i < len_s1; i++)
+		concatenated[i] = s1[i];
 
-    for (j = 0; j < n; j++)
-        concatenated[i + j] = s2[j];
+	for (j = 0; j < n; j++)
+		concatenated[i + j] = s2[j];
 
-    concatenated[i + j] = '\0';
+	concatenated[i + j] = '\0';
 
-    return concatenated;
+	return concatenated;
 }
