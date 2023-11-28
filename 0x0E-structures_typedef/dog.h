4c0933de440da2e8278611119e2eb95d44d28b5a
Agbenyegah1 <kofysmile123@gmail.com>
2023-11-27 16:10:56 -0800

    o.poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,28 @@
 #include "dog.h"
-/**
- * Initializes a struct dog with the specified name, age, and owner
- *
- * @param d: Pointer to the struct dog to initialize
- * @param name: Dog's name
- * @param age: Dog's age in years
- * @param owner: Dog's owner's name
- *
- * Return: void
+/*
+ * Define a new type of strut Dog with the following membets 
+ * name : A pointer to character array that represents the dog's name
+ * age : A float that represent the dogs age in years
+ * owner : A pointer to a characted array that represents the owner's name
+ * 
  */
-void initialize_dog(struct dog *d, char *name, float age, char *owner) {
-    // Verify if the pointer is valid
-    if (d) {
-        // Set the dog's name
-        d->name = name;
-
-        // Set the dog's age
-        d->age = age;
-
-        // Set the dog's owner
-        d->owner = owner;
-    }
+typedef struct dog{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
+/* @param d : Pointer to the struct dog to intialize 
+ * @param name : Dog's name
+ * @param age : Dog's age 
+ * @param owner : Dog's  owner
+ *
+ * return void
+ *
+ */
+void initialize_dog(dog_t *d,char *name,float age,char *owner){
+	if (d){
+		d-->name = name;
+			d--> age= age; 
+		d--> owner= owner;
+	}
 }
