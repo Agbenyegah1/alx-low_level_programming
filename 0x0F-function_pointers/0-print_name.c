d69539ee4f9580131fccbe6692cdc9393e8ebe7c
Agbenyegah1 <kofysmile123@gmail.com>
2023-11-29 16:45:59 -0800

    functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,9 +7,11 @@
  *
  * Rteurn: void
  */
-void print_name(char *name, void (*f)(char *))
-{
-	if (name != NULL && f != NULL)
+#include "function_pointers.h"
 
-		f(name);
+void print_name(char *name, void (*f)(char *)) {
+  if (name && f) {
+    f(name);
+  }
 }
+
