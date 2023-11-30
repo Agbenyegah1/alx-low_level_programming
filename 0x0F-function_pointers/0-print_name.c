88ebf407dedac0ca2785d58192cc4f2144d04875
Agbenyegah1 <kofysmile123@gmail.com>
2023-11-29 16:37:06 -0800

    functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,13 @@
-#include "function_pointer.h"
+#include "function_pointers.h"
 /**
  * print_name - Prints a name using a function pointer.
  * @name: Name of the person.
  * @f: Function pointer to a function that prints a name.
  */
 void print_name(char *name, void (*f)(char *)) {
-  // Check if the function pointer is valid and call it if so.
-  f ? f(name) : ;
+  // Check if the function pointer is valid.
+  if (f) {
+    // Call the function pointed to by f.
+    f(name);
+  }
 }
